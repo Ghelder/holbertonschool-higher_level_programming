d829640143f6eb8bd30906e9cc9944d4176149ab
Ghelder <Jhelder@outlook.com>
2023-06-03 08:11:19 -0700

    fix: changing the order of the algorithm

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,11 +4,12 @@ if __name__ == "__main__":
 
     from sys import argv
 
-    if (len(argv) - 1) == 0:
+    if (len(argv)) == 1:
         print("0 arguments.")
-    elif (len(argv) - 1) == 1:
+    elif (len(argv)) == 2:
         print("1 argument:")
+        print("1: {}".format(argv[1]))
     else:
         print("{} arguments".format(len(argv) - 1))
-    for i in range(len(argv) - 1):
-        print("{}: {}".format(i + 1, argv[i + 1]))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
