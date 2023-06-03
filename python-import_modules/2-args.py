24d6168b4156b589eb4ff5b13a331962dbf72942
Ghelder <Jhelder@outlook.com>
2023-06-03 07:58:06 -0700

    fix: moving the import line

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-from sys import argv
 
 if __name__ == "__main__":
+
+    from sys import argv
 
     if (len(argv) - 1) == 0:
         print("0 arguments.")
     elif (len(argv) - 1) == 1:
         print("1 argument:")
     else:
-        print("{} argument:".format(len(argv) - 1))
-        for i in range(len(argv) - 1):
-            print("{}: {}".format(i + 1, argv[i + 1]))
+        print("{} arguments".format(len(argv) - 1))
+    for i in range(len(argv) - 1):
+        print("{}: {}".format(i + 1, argv[i + 1]))
