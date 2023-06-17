e9df5c48ae2976ddd564bb9df00a760c344c98b5
Ghelder <Jhelder@outlook.com>
2023-06-17 04:49:29 -0700

    feat: square class calculates the area

---
+++ python-classes/3-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""square class definition"""
+
+
+class Square:
+    """square class with private attribute instance
+    Attributes:
+        __size: instance private attribute
+    """
+
+    def __init__(self, size=0):
+        """initializing the instance
+
+        valid:
+            size must be an integer
+            if size is less than 0
+
+        Args:
+            __size: defines the private instance
+            size(int): size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """public method calculate the square area
+        Returns:
+            square area
+        """
+        return self.__size * self.__size
