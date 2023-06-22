c11adf0e4217fbe7533cf73c2b8c2eda09828dcb
Ghelder <Jhelder@outlook.com>
2023-06-22 15:52:34 -0700

    feat: add method area

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry."""
+
+
+class BaseGeometry:
+    """class BaseGeometry based."""
+
+    def area(self):
+        """Method instance area.
+
+        Returns:
+            Exception: area() is not implemented
+
+        """
+        return Exception("area() is not implemented")
