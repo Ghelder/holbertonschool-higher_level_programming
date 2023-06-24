eb02b2fc59e5f3ab0c7e5748a2a416afb715144d
Ghelder <Jhelder@outlook.com>
2023-06-24 16:04:29 -0700

    feat: add style docstrings

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """Defines a function return the JSON representation of an object (string)."""
+import json
 
 
 def to_json_string(my_obj):
-    """Returns the JSON representation of an object (string)."""
-    import json
+    """Returns the JSON representation of an object (string).
+
+    Args:
+        my_obj (object): The object to convert.
+
+    Returns:
+        The JSON representation of an object (string)."""
 
     return json.dumps(my_obj)
