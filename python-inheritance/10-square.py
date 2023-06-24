25bc985ccefacabdded226c4a42be1db79cf7032
Ghelder <Jhelder@outlook.com>
2023-06-24 04:00:07 -0700

    feat: Adding a Square class with an __init__ magic constructor method

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a class Square that inherits from Rectangle."""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Class Square based on Rectangle."""
+    def __init__(self, size):
+        """Initialize a new Square."""
+        self.__size = size
+        super().integer_validator("size", size)
+        super().__init__(size, size)
