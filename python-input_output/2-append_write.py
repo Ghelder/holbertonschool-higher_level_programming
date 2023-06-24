cfaecbf8f43563a32562c5c03a5f02542da591e8
Ghelder <Jhelder@outlook.com>
2023-06-24 06:58:52 -0700

    fix: new-line docstrings

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a function that appends a string at the end of a text file."""
 
+
 def append_write(filename="", text=""):
     """Prints the contents of a text file.
 
