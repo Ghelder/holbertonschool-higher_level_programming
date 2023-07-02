752e04d0363f915487c42b8f3adfd2f0c576cbff
Ghelder <Jhelder@outlook.com>
2023-07-02 10:03:16 -0700

    fix: validating instance

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -53,6 +53,9 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """Returns an instance with all attributes already set."""
-        dummy_instance = cls(1, 1)
-        dummy_instance.update(**dictionary)
-        return dummy_instance
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
